380db9d884e96c7df7d20cd3d5ccf01b12836c6e
Playdeads <gkaua4380@gmail.com>
2024-05-14 17:22:00 -0300

    feat: bolinha de carrossel

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-
-
 let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
-let iconeX = document.querySelector("#icone-x")
-
+let iconeX = document.querySelector("#xlogo")
 function abrirFecharMenu() {
-    // se o menu está fechado
-    if (menu.classList.contains("menu-fechado")) {
-
-        // abrir o meu
+    if(menu.classList.contains("menu-fechado")){
         menu.classList.remove("menu-fechado")
-
-        // mostrar icone x
         iconeX.style.display = "inline"
-
-        // esconder icone barras
         iconeBarras.style.display = "none"
-
     } else {
-
-        // fechar o menu
         menu.classList.add("menu-fechado")
-
-        // mostrar icone x
         iconeX.style.display = "none"
-
-        // esconder icone barras
         iconeBarras.style.display = "inline"
     }
-
 }
 
 window.onresize = () => {
-    menu.classList.remove ("menu-fechado")
-    iconeX.style.display = "inline"
+    menuclassList.remove("menu-fechado")
+    icone.style.display = "inline"
     iconeBarras.style.display = "none"
+}
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numeroSlide = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtualm < (numeroSlide - 1 )) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostraSlideAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+        slideAtual = numeroSlide - 1 
+    }
+    
+
+    banner.classList.add(slides[slideAtual])
+
 }
